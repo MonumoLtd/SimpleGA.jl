885be0f810acced047ad257c1279b3aa2b13824a
Documenter.jl <documenter@juliadocs.github.io>
2022-10-17 10:47:29 +0000

    build based on ab2570a

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = GADraft","category":"page"},{"location":"#GADraft","page":"Home","title":"GADraft","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for GADraft.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [GADraft]","category":"page"}]
+}
