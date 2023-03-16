68418013a892bdec548291dbd3ddddb864941c07
Chris Doran <chrisjldoran@gmail.com>
2023-03-16 16:35:12 +0000

    Added CGA

--- test/runtest.jl
+++ test/runtest.jl
@@ -8,12 +8,12 @@ using Test
     include("test20.jl")
     include("test30.jl")
     include("testSTA.jl")
-    #include("test40.jl")
-    #include("testPGA.jl")
-    #include("testCGA.jl")
+    include("test40.jl")
+    include("testPGA.jl")
+    include("testCGA.jl")
     #include("test33.jl")
     #include("test24.jl")
-    #include("test44.jl")
+    include("test44.jl")
     #include("test64.jl")
 end
 
