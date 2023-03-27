b501f9fe21a8e504027723e62cce4e7c36bac339
Tom Gillam <tom.gillam@monumo.com>
2023-03-27 13:18:39 +0100

    Add docstrings

--- test/testfuncs.jl
+++ test/testfuncs.jl
@@ -1,6 +1,6 @@
-#Common functions used in testing.
+# Common functions used in testing.
 
-#Function to test off-diagonal basis elements all vanish.
+"""Test that off-diagonal basis elements all vanish."""
 function testbas(bas)
     res = true
     n = length(bas)
@@ -12,19 +12,19 @@ function testbas(bas)
     return res
 end
 
-#Test shared across all algebras.
+"""Test common features of all algebras."""
 function run_common_tests(me1, me2, me3, mo1, mo2, mo3, v1, v2)
-    #Addition
+    # Addition
     @test isapprox(1.0 + me1, me1 + 1.0)
     @test isapprox(-1.0 + me1, me1 - 1.0)
 
-    #Distributivity
+    # Distributivity
     @test isapprox(me1 * (me2 + me3), me1 * me2 + me1 * me3)
     @test isapprox(mo1 * (me2 + me3), mo1 * me2 + mo1 * me3)
     @test isapprox(me1 * (mo2 + mo3), me1 * mo2 + me1 * mo3)
     @test isapprox(mo1 * (mo2 + mo3), mo1 * mo2 + mo1 * mo3)
 
-    #Associativity
+    # Associativity
     @test isapprox(me1 * (me2 * me3), (me1 * me2) * me3)
     @test isapprox(mo1 * (me2 * me3), (mo1 * me2) * me3)
     @test isapprox(me1 * (mo2 * me3), (me1 * mo2) * me3)
@@ -34,7 +34,7 @@ function run_common_tests(me1, me2, me3, mo1, mo2, mo3, v1, v2)
     @test isapprox(me1 * (mo2 * mo3), (me1 * mo2) * mo3)
     @test isapprox(mo1 * (mo2 * mo3), (mo1 * mo2) * mo3)
 
-    #Projection
+    # Projection
     @test isapprox(
         me1,
         project(me1, 0) +
@@ -47,7 +47,7 @@ function run_common_tests(me1, me2, me3, mo1, mo2, mo3, v1, v2)
         mo1, project(mo1, 1) + project(mo1, 3) + project(mo1, 5) + project(mo1, 7)
     )
 
-    #Rotation
+    # Rotation
     R = bivector_exp(v1 * v2)
     no1 = R * mo1 * R'
     no2 = R * mo2 * R'
@@ -56,7 +56,7 @@ function run_common_tests(me1, me2, me3, mo1, mo2, mo3, v1, v2)
     ne2 = R * me2 * R'
     @test isapprox(dot(me1, me2), dot(ne1, ne2))
 
-    #Reverse
+    # Reverse
     @test isapprox((me1 + me1') / 2, tr(me1) + project(me1, 4))
     @test isapprox((me1 - me1') / 2, project(me1, 2) + project(me1, 6))
     @test isapprox((mo1 + mo1') / 2, project(mo1, 1) + project(mo1, 5))
